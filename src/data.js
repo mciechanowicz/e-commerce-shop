828d0acb9871f0dab42ff429862683ec141031d7
mciechanowicz <mciechanowicz94@gmail.com>
2022-01-11 21:00:04 +0000

    fill data

--- src/data.js
+++ src/data.js
@@ -25,13 +25,13 @@ export const sliderData = [
 export const categories = [
   {
     id: 1,
-    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img: 'https://images.pexels.com/photos/9972804/pexels-photo-9972804.jpeg?cs=srgb&dl=pexels-vitaliy-izonin-9972804.jpg&fm=jpg',
     title: 'SHIRT STYLE!',
   },
   {
     id: 2,
-    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    title: 'LOUNGEWEAR LOVE',
+    img: 'https://images.pexels.com/photos/7948616/pexels-photo-7948616.jpeg?cs=srgb&dl=pexels-los-muertos-crew-7948616.jpg&fm=jpg',
+    title: 'DRESS LOVE',
   },
   {
     id: 3,
@@ -40,12 +40,12 @@ export const categories = [
   },
   {
     id: 4,
-    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    title: 'LOUNGEWEAR LOVE',
+    img: 'https://images.pexels.com/photos/3782789/pexels-photo-3782789.jpeg?cs=srgb&dl=pexels-bella-zhong-3782789.jpg&fm=jpg',
+    title: 'UNUSUAL SHOES',
   },
   {
     id: 5,
-    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    title: 'LIGHT JACKETS',
+    img: 'https://images.pexels.com/photos/8991390/pexels-photo-8991390.jpeg?cs=srgb&dl=pexels-jose-musa-8991390.jpg&fm=jpg',
+    title: 'SUMMER HATS',
   },
 ]
